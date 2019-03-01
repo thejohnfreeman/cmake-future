f7af9df6748ac52107f4eaae17855a55877484a8
John Freeman <jfreeman08@gmail.com>
2019-02-28 20:04:42 -0600

    Fix ExportDir for Windows

--- ExportDir.cmake
+++ ExportDir.cmake
@@ -3,7 +3,11 @@
 # which is empty by default and should be assigned only by projects.
 #
 # The path searched by `find_package` differs by platform:
-# https://cmake.org/cmake/help/v3.6/command/find_package.html
+# https://cmake.org/cmake/help/latest/command/find_package.html
+# But it is derived from the installation prefixes held in the variable
+# `CMAKE_SYSTEM_PREFIX_PATH`:
+# https://cmake.org/cmake/help/latest/variable/CMAKE_SYSTEM_PREFIX_PATH.html#variable:CMAKE_SYSTEM_PREFIX_PATH
+#
 # We use a switch on platform (`UNIX` and `WIN32`) to choose the right
 # installation path. We must set a value that works with the intended usage:
 #
@@ -11,11 +15,8 @@
 #       DESTINATION ${CMAKE_INSTALL_EXPORTDIR}/${PROJECT_NAME}-${PROJECT_VERSION}
 #     )
 #
+list(GET CMAKE_SYSTEM_PREFIX_PATH 0 PREFIX)
 if(${UNIX})
-  include(GNUInstallDirs)
-  set(CMAKE_INSTALL_EXPORTDIR ${CMAKE_INSTALL_LIBDIR}/cmake)
-elseif(${WIN32})
-  set(CMAKE_INSTALL_EXPORTDIR ${CMAKE_INSTALL_PREFIX})
-else()
-  message("unknown platform; CMAKE_INSTALL_EXPORTDIR not set")
+  set(PREFIX "${PREFIX}/lib")
 endif()
+set(CMAKE_INSTALL_EXPORTDIR "${PREFIX}/cmake")
