bf191711345e43e348bdaf3dc1dbfdd50f5219c0
John Freeman <jfreeman08@gmail.com>
2019-05-10 11:15:35 -0500

    Better explain FutureExportDir in comment

--- src/FutureExportDir.cmake
+++ src/FutureExportDir.cmake
@@ -1,12 +1,19 @@
-# Third-party CMake extension modules must be imported with `find_package`, not
-# `include`. `include` searches the built-in modules and `CMAKE_MODULE_PATH`,
-# which is empty by default and should be assigned only by projects.
+# This extension exports a variable, `CMAKE_INSTALL_EXPORTDIR`, that points to
+# the prefix where CMake package configuration files (and their corresponding
+# `<PackageName>-targets.cmake` *export* files) should be installed. It is on
+# the path where `find_package` searches for package configuration
+# files. It differs by platform, but it is derived from the installation
+# prefixes held in the variable `CMAKE_SYSTEM_PREFIX_PATH`.
 #
-# The path searched by `find_package` differs by platform:
-# https://cmake.org/cmake/help/latest/command/find_package.html
-# But it is derived from the installation prefixes held in the variable
-# `CMAKE_SYSTEM_PREFIX_PATH`:
-# https://cmake.org/cmake/help/latest/variable/CMAKE_SYSTEM_PREFIX_PATH.html#variable:CMAKE_SYSTEM_PREFIX_PATH
+# [package configuration file]: https://cmake.org/cmake/help/latest/manual/cmake-packages.7.html#package-configuration-file
+# [find_package]: https://cmake.org/cmake/help/latest/command/find_package.html
+# [`CMAKE_SYSTEM_PREFIX_PATH`]: https://cmake.org/cmake/help/latest/variable/CMAKE_SYSTEM_PREFIX_PATH.html#variable:CMAKE_SYSTEM_PREFIX_PATH
+#
+# (CMake has the concept of "package registry" which sounds like it should be
+# the preferred location to install package configuration files, but I have
+# been told by insiders that it is not encouraged.)
+#
+# https://cmake.org/cmake/help/v3.14/manual/cmake-packages.7.html#package-registry
 #
 # We use a switch on platform (`UNIX` and `WIN32`) to choose the right
 # installation path. We must set a value that works with the intended usage:
@@ -15,6 +22,13 @@
 #       DESTINATION ${CMAKE_INSTALL_EXPORTDIR}/${PROJECT_NAME}-${PROJECT_VERSION}
 #     )
 #
+# This installation path is useful for more than just package configuration
+# files; it is necessary for third-party CMake extension modules (like this
+# one). `include` searches the built-in modules and `CMAKE_MODULE_PATH`, which
+# is empty by default and should be assigned only by projects. There is no
+# place to install extension modules where they can be discovered by default,
+# i.e. without requiring projects to set `CMAKE_MODULE_PATH`. The only
+# alternative import function is `find_package`.
 
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
   # Unless we join these into the same message, they get separated by a stack
