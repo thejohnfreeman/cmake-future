536c32c66b0a97962436f5ba099035d960912cb5
John Freeman <jfreeman08@gmail.com>
2019-07-02 10:02:38 -0500

    Add expected failure tests

--- tests/test_projects.py
+++ tests/test_projects.py
@@ -5,7 +5,8 @@ import pytest  # type: ignore
 
 
 @pytest.mark.parametrize('project', ['kitchen_sink'])
-def test_project(tmp_path, project):
+def test_good_project(tmp_path, project):
+    """Test expected successes."""
     print(f'tmp_path = {tmp_path}')
     sp.run(
         [
@@ -17,3 +18,22 @@ def test_project(tmp_path, project):
         cwd=tmp_path,
         check=True,
     )
+
+
+# Expected failures.
+@pytest.mark.parametrize('project', ['no_license'])
+def test_bad_project(tmp_path, project):
+    """Test expected failures."""
+    # TODO: We need to make sure these fail in the expected way.
+    print(f'tmp_path = {tmp_path}')
+    with pytest.raises(sp.CalledProcessError):
+        sp.run(
+            [
+                'cmake',
+                '-Wdev',
+                '-Werror=dev',
+                str(Path(f'tests/projects/{project}').resolve()),
+            ],
+            cwd=tmp_path,
+            check=True,
+        )
