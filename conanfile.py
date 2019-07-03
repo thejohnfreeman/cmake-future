940480fa8da71a1c26eb9674fa9bc88426f35cd4
John Freeman <jfreeman08@gmail.com>
2019-07-03 00:09:18 -0500

    Do not try to collect cpp_info

--- conanfile.py
+++ conanfile.py
@@ -7,5 +7,8 @@ class Recipe(CMakeConanFile):
     name = CMakeConanFile.__dict__['name']
     version = CMakeConanFile.__dict__['version']
 
+    def package_info(self):
+        pass
+
     def package_id(self):
         return self.info.header_only()
